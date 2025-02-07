f5b26a581e0883d1b2a80492cb307880bfdc7f1c
Lela Tarniah <la.tarniah@gmail.com>
2025-02-07 18:30:47 -0500

    worked on login page

--- FrontEnd/js/login.js
+++ FrontEnd/js/login.js
@@ -2,15 +2,16 @@
 //TODO integrate login page
 // fetch("http://localhost:5678/api/users/login", {
 
-
-async function login(emailAddress, password) {
+//TODO add click event listener to submit button wich call the function below
+async function login(email, password) {
+    // FIXME get email and pass from input field bc its going coming as paramaters
     try {
         const response = await fetch('("http://localhost:5678/api/users/login"', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({ emailAddress, password })
+            body: JSON.stringify({ email, password })
         });
 
         if (response.ok) {
